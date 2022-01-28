f5fc0ec990823d1c175d2fa89d616a26b463509d
Yan Rodrigues <ccyan05@alu.ufc.br>
2022-01-28 00:04:42 -0300

    Connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,13 +1,12 @@
-// const db_config = require("../config/database_config.js");
-const conf = require("../config/database_config.js");
+const db_config = require("../config/database_config.js");
 const {Client} = require("pg");
 
 const client = new Client({
-  host: conf.host,
-  user: conf.user,
-  port: conf.port,
-  password: conf.password,
-  database: conf.database
+  host: db_config.host,
+  user: db_config.user,
+  port: db_config.port,
+  password: db_config.password,
+  database: db_config.database
 });
 
 client.connect();
