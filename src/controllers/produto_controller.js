007bda03478abb60dcb5b4ec26e9666bd7a48224
Yan Rodrigues <ccyan05@alu.ufc.br>
2022-01-28 15:02:41 -0300

    Adding put method

--- src/controllers/produto_controller.js
+++ src/controllers/produto_controller.js
@@ -1,3 +1,4 @@
+const { client_encoding } = require("pg/lib/defaults");
 const client = require("../database/connection.js");
 
 module.exports = {
@@ -6,21 +7,46 @@ module.exports = {
       const response = await client.query("SELECT * FROM produto");
       res.status(200).json(response.rows);
     } catch (err) { 
-      console.err(err);
+      console.error(err);
     }
   },
   async store(req, res) {
     try {
-      
+      const {id, preco, qtd, nome, id_fornecedor} = req.body;
+      const response = await client.query(`INSERT INTO produto 
+      (id_produto, preco, quantidade, nome, idfornecedor)
+      VALUES ('${id}', '${preco}', '${qtd}', '${nome}', '${id_fornecedor}')
+      `);
+      res.json(response.rows);
+      await client.end();
     } catch (err) {
       console.error(err);
     }
   },
   async delete_by_id(req, res) {
     try {
-      
+      const {id} = req.body;
+      const response = await client.query(`
+      DELETE FROM produto 
+      WHERE id_produto = '${id}'`);
+      res.status(200).json(response.rows);
+      await client.end();
+    } catch (err) {
+      console.error(err);
+    }
+  },
+  async update_preco_quantidade_by_id(req, res) {
+    try {
+      const {id, preco, qtd} = req.body;
+      const response = await client.query(`
+        UPDATE produto
+        SET preco = '${preco}', quantidade = '${qtd}'
+        WHERE id_produto = '${id}'
+      `)
+      res.status(200).json(response.rows);
+      await client.end();
     } catch (err) {
       console.error(err);
     }
   }
-}
+} 
