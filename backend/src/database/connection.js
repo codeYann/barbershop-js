c5111ffd3d3934936e2ec5fdd0c06ebaa1ca57ae
Yan Rodrigues <ccyan05@alu.ufc.br>
2022-01-30 00:07:21 -0300

    Change client to pool connection

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,14 +1,12 @@
 const database_env = require("../config/database_config.js");
-const { Client } = require("pg");
+const { Pool } = require("pg");
 
-const client = new Client({
+const client = new Pool({
   host: database_env.host,
   user: database_env.user,
   port: database_env.port,
   password: database_env.password,
   database: database_env.database,
 });
-
-client.connect();
 
 module.exports = client;
