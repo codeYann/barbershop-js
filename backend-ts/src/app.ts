8c325125dda876be7ecea50d5f8c656366163c99
codeYann <ccyan05@alu.ufc.br>
2022-03-20 14:24:01 -0300

    App.ts

---
+++ backend-ts/src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import routes from "./routes";
+
+class App {
+  public express: express.Application;
+
+  public constructor() {
+    this.express = express();
+    this.middlewares();
+  }
+
+  private middlewares(): void {
+    this.express.use(express.json());
+    this.express.use(cors());
+    this.express.use(routes);
+  }
+}
+
+export default new App().express;
