ef8301250f174176f246c8e4868e86f73eb927bc
codeYann <ccyan05@alu.ufc.br>
2022-03-20 14:24:34 -0300

    Server.ts

---
+++ backend-ts/src/server.ts
@@ -0,0 +1,7 @@
+import App from "./app";
+import dotenv from "dotenv";
+
+dotenv.config();
+App.listen(process.env.PORT, (): void => {
+  console.table([`Server Running on localhost:${process.env.PORT}`]);
+});
