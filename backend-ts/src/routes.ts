e29204123241982c6ef483d159600084abfe9985
codeYann <ccyan05@alu.ufc.br>
2022-03-31 10:13:49 -0300

    Moving class Router to routes var

--- backend-ts/src/routes.ts
+++ backend-ts/src/routes.ts
@@ -1,18 +1,9 @@
 import {Request, Response, Router} from 'express';
 import clienteController from './controllers/clientesController';
 
-class Routes {
-  public router: Router;
+const router = Router();
 
-  public constructor() {
-    this.router = Router();
-    this.getRoutes();
-  }
+router.get('/', (req: Request, res: Response) => res.json({}));
+router.get('/clientes', new clienteController().index);
 
-  public getRoutes(): void {
-    this.router.get('/', (req: Request, res: Response) => res.json({}));
-    this.router.get('/clientes', new clienteController().index);
-  }
-}
-
-export default new Routes().router;
+export default router;
